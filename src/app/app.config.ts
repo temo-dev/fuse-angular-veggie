ab2b30bd2c3278955f9159b91e8c037b04f81775
Daniel Nguyen <dev.temo2020@gmail.com>
2024-12-18 12:32:46 +0100

    add-base-project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -85,7 +85,7 @@ export const appConfig: ApplicationConfig = {
                 services: mockApiServices,
             },
             fuse   : {
-                layout : 'classy',
+                layout : 'classic',
                 scheme : 'light',
                 screens: {
                     sm: '600px',
